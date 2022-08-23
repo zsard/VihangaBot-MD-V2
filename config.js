87d7e5e1e56a9b0e9d185b624a583143761e1da7
zsard <111820533+zsard@users.noreply.github.com>
2022-08-23 05:55:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,18 +18,18 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '94760107265' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
+global.owner = ['94760107265',''] //මෙතන number එක.
+global.premium = ['94760107265','94760107264'] //මෙතන number එක.
+global.botname = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐛𝐨𝐭' //මෙතන නම.
+global.ownername = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐁𝐎𝐓' //මෙතන නම.
+global.ownername = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐁𝐎𝐓😇' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/JqP2KpFNzj1LPt4L56WtJ4' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/JqP2KpFNzj1LPt4L56WtJ4' //මෙතන grouplink එක.
+global.packname = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐁𝐎𝐓' //මෙතන නම.
+global.cap = '*By  𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐁𝐎𝐓*' //මෙතන නම.
 global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
 global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
@@ -52,7 +52,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = '𝐒𝐀𝐂𝐇𝐈𝐘𝐀 𝐁𝐎𝐓' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
